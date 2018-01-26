ac948a54e5f26da301c87c27b4cb644600c277fb
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-26 08:47:53 -0800

    add jquery,bootstrap,scripts.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Pizza(){
+  this.add[]; //add toppings and size together and shows how much their going to spend
+  this.totalScore[];//gives them their total cost
+}
+//add the pizza sizes and toppings and push it to the add array.
+Pizza.prototype.Select
+
+
+
+
+//give the user the total cost
+Pizza.prototype.cost
