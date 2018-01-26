5a7f82ff12d9c0d6c942b00a0322819b13f098bf
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-26 11:22:27 -0800

    add to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-function Pizza(topping, size) {
-    this.topping = topping;
+function Pizza(size, toppings) {
     this.size = size;
+    this.toppings = toppings;
+
 }
 
 function pizzaCost() {
-  var toppings = $("$#toppings").val();
-  var size = $("$#size")
+  var size = $("#size").val();
+  var toppings = $("#toppings").val();
+  var newCost = new Pizza(size, toppings);
 
+  return newCost
 }
+
+
+Pizza.prototype.Cost = function(){
+  var cost =0;
+ if (this.size === "small" || this.size === "medium"){
