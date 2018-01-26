14e61f328ae1aa4eaf0c2f176762bce9158c97a0
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-26 10:49:24 -0800

    add to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,10 @@
-function Pizza(){
-  this.add[]; //add toppings and size together and shows how much their going to spend
-  this.totalScore[];//gives them their total cost
-}
-//add the pizza sizes and toppings and push it to the add array.
-Pizza.prototype.Select = function(){
-    var toppingPrices = 
-
-
-
-
+function Pizza(topping, size) {
+    this.topping = topping;
+    this.size = size;
 }
 
+function pizzaCost() {
+  var toppings = $("$#toppings").val();
+  var size = $("$#size")
 
-
-
-//give the user the total cost
-Pizza.prototype.cost
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//front-end logic
+}
